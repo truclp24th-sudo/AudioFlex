efb97fc7692501d7e49f64258d69fcd2bd3c843e
truclp24th-sudo <truclp.24th@sv.dla.edu.vn>
2026-07-19 14:53:10 +0700

    Lab_11: Chức năng quản lý sản phẩm

---
+++ Lab11/Models/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AudioFlex.Models
+{
+    public class ProductImage
+    {
+        [Key]
+        public int ProductImageId { get; set; }
+
+        [Required]
+        public string ImagePath { get; set; } // Lưu tên file ảnh (vd: th11-be.jpg)
+
+        public string? ColorName { get; set; } // Lưu tên màu nếu cần (vd: Trắng, Đen)
+
+        // Khóa ngoại liên kết tới bảng Product
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
