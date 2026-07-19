86e9e441839eb9b1434e70b2ffda673a6020a8cb
truclp24th-sudo <truclp.24th@sv.dla.edu.vn>
2026-07-19 14:11:48 +0700

    Lab_1: Thiết lập & tạo môi trường CSDL

---
+++ Lab01/Models/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace AudioFlex.Models
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ImagePath { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total => Price * Quantity;
+    }
+}
